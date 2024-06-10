b5c08fb99d0a53ddc15280ba9b07b7940c614298
Gee1818 <g1sw@hotmail.com>
2024-06-10 00:46:26 +0000

    aa

---
+++ src/ambiente/301_GrabarMisSemillas.r
@@ -0,0 +1,14 @@
+# Este script almacena definitivamente sus cinco semillas
+# en el bucket, de forma que NO deba cargarlas en cada script
+
+require( "data.table" )
+
+# reemplazar aqui por SUS semillas 
+mis_semillas <- c(276277, 311609, 520067, 535571, 696343)
+
+tabla_semillas <- as.data.table(list( semilla = mis_semillas ))
+
+fwrite( tabla_semillas,
+    file = "~/buckets/b1/datasets/mis_semillas.txt",
+    sep = "\t"
+)
